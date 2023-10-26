13b8dbec970a1e1cdee15e993e1846e53e060c5f
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-10-26 12:59:13 +0900

    231026 [Add] 할일 입력, 리스트 체크박스

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  import Todo from './components/Todo.vue';
+  export default{
+    components: {
+      Todo
+    },
+    data(){
+      return{
+        todoText: '',
+        todos: [
+          {id:1, text:'Learn Web', checked:false},
+          {id:2, text:'Get a job', checked:false}
+        ]
+      }
+    },
+    methods: {
+      AddTodo(e){
+        // console.log(e.target.value)
+        this.todos.push({
+          id: Math.random(),
+          text: e.target.value,
+          checked: false
+        });
+        this.todoText = '';
+      },
+      toggleCheck({id, checked}){
+        const index = this.todos.findIndex(todo => todo.id === id);
+        this.todos[index].checked = checked;
+        console.log(this.todos);
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">Todo App</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="mb-3">
+      <label for="todo" class="form-label">Todo Input</label>
+      <input 
+        type="text" 
+        class="form-control" 
+        id="todo" 
+        placeholder="할 일을 입력하세요"
+        required
+        @keyup.enter="AddTodo"
+        v-model="todoText"
+      >
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" @toggle-checkbox="toggleCheck"/>
+
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,33 @@
+<script>
+  export default {
+    props: {
+      todo: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      toggleCheckbox(e){
+        this.$emit('toggle-checkbox',{
+          id: this.todo.id,
+          checked: e.target.checked
+        })
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="form-check" :data-id="todo.id">
+    <input 
+      class="form-check-input" 
+      type="checkbox" 
+      value="" 
+      :id="'input'+todo.id"
+      @change="toggleCheckbox"
+    >
+    <label class="form-check-label" :for="`input${todo.id}`">
+      {{ todo.text }}
+    </label>
+  </div>
+</template>
